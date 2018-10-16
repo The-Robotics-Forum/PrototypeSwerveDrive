bcaf44a836633346e2157404784cc41720989b1b
Varun Gujarathi <v.gujarathi777@gmail.com>
2018-10-16 19:07:49 +0530

    Initial Commit

--- PrototypeSwerveDrive/PrototypeSwerveDrive.cpp
+++ PrototypeSwerveDrive/PrototypeSwerveDrive.cpp
@@ -1,16 +1,22 @@
 /*
  * PrototypeSwerveDrive.cpp
- *
  * Created: 16-10-2018 18:48:07
- *  Author: varungujarathi9
+ *
+ * //TODO:	1. Include servo.h
+			2. Attach 2 servos on same pin
+			3. Control angle using Android App
  */ 
 
-#include <avr/io.h>
+#include <m2560/io.h>
 
 int main(void)
 {
+	pinMode(13,OUTPUT);
     while(1)
     {
-        //TODO:: Please write your application code 
-    }
+		digitalWrite(13,HIGH);
+		_delay_ms(1000);
+		digitalWrite(13,LOW);
+		_delay_ms(1000);
+	}		 
 }
