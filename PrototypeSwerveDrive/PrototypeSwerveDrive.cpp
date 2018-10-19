e0219d99d049ab4aa155523cb9fda42bed2acde5
Varun Gujarathi <v.gujarathi777@gmail.com>
2018-10-20 03:03:57 +0530

    Update cpp file

--- PrototypeSwerveDrive/PrototypeSwerveDrive.cpp
+++ PrototypeSwerveDrive/PrototypeSwerveDrive.cpp
@@ -22,41 +22,19 @@
 #include <m2560/io.h>
 #include <m2560/Servo.h>
 //#include <ModularLibrary/Cytron.h>
+/********************Objects***********************/
 Servo swerve1;
-/*#include <avr/io.h>
-#include <math.h>
-#include <stdlib.h>
-#include <util/delay.h>*/
-int angle[4]={135,135,135,135};
+Servo swerve2;
+
+/********************Variables********************/
+int angle[4]={180,135,135,135};
 	
 void setup(){
-int x;	
-DDRB=0xff;
 Serial.begin(4800);
 swerve1.attach(11);
+swerve2.attach(12);
 }
-void loop()
-{
-	/*x=map(15,10,20,30,40);*/
-	for(int i=1;i<270;i+=10){
-	swerve1.write(i);
-	_delay_ms(500);	
-	}
-	
-	/*swerve1.write(180);
-	_delay_ms(2000);*/
-	//DDRD   |= (1<<PD5);
-		/*TCCR1A |= (1<<WGM11) | (1<<COM1A1) | (1<<COM1A0);
-		TCCR1B |= (1<<WGM12) | (1<<WGM13);
-		TCCR1B |= (1<<CS10);
-		//val=90;
-		//val=map(val,0,240,17700,19530)
-		ICR1 =20000;
-		
-		//OCR1A = ICR1-(1500-(690*5.55555555));
-		OCR1A=18600;
-		/*PORTB=0xff;
-		_delay_ms(1000);
-		PORTB=0x00;
-		_delay_ms(1000);*/
+void loop(){
+	swerve2.write(angle[1]);
+	swerve1.write(angle[0])
 }
